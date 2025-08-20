a1fdca73e64e108fcfe4ed13e54ef4dd8baf7cd8
yuanjasper <yuanjasper14@gmail.com>
2025-08-20 16:15:10 +0800

    Added repeat and bye

--- src/main/java/Daco.java
+++ src/main/java/Daco.java
@@ -1,6 +1,11 @@
+import java.util.*;
+
 public class Daco {
-    static String LINESEP = "____________________________________________________________";
+    static String LINESEP = "____________________________________________________________\n";
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String neutralfaces[] = {"(´⌣`ʃƪ)", "| (• ◡•)|", "(◌˘◡˘◌)"};
+        String sadfaces[] = {"（◞‸◟）", "へ[ •́ ‸ •̀ ]ʋ"};
         String logo;
         logo = " ____                  \n" +
                 "|  _ \\  __ _  ___ ___  \n" +
@@ -9,6 +14,19 @@ public class Daco {
                 "|____/ \\__,_|\\___\\___/ \n";
         System.out.println(logo + LINESEP);
         System.out.println("Hi there, I'm Daco! How can I help.\n" + LINESEP);
-        System.out.println("Come back anytime.\n" + LINESEP);
+
+        while (true) {
+            String userinput = sc.nextLine();
+            if (userinput.equals("bye")) {
+                break;
+            }
+            System.out.println(LINESEP + userinput + " " + selectface(neutralfaces) + "\n" + LINESEP);
+        }
+        System.out.println(LINESEP + "Come back anytime. " + selectface(sadfaces) +"\n" + LINESEP);
+    }
+
+    public static String selectface(String[] emotions) {
+        Random random = new Random();
+        return emotions[random.nextInt(emotions.length)];
     }
 }
